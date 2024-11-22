a3b202c3beda4802cadf012d75e65531385ca247
ElianTrem <eliantreminio11@gmail.com>
2024-11-21 19:31:34 -0600

    INI: push inicial

---
+++ src/main/java/com/example/bingo/bingo_api/BingoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bingo.bingo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BingoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bingo/bingo_api/BingoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bingo.bingo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BingoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
